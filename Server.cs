8f4528ae48546815d6a65c21597960f6c1ef5494
wheetlan <whtl@ya.ru>
2018-05-21 17:35:11 +0300

    Добавлен класс Server (ещё не заполнен)

---
+++ Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Server
+{
+        private string host;
+        private int port;
+        private bool stream_enable;
+        private const int CHUNK_SIZE = 8;
+
+        public Client(string host, int port, bool stream_enable)
+        {
+            this.host = host;
+            this.port = port;
+            this.stream_enable = stream_enable;
+        }
+
+      
+
+
+    }
+}
