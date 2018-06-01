89d46b7bd86bc2f2266941caafd64e81223b5484
wheetlan <whtl@ya.ru>
2018-06-01 21:42:29 +0300

    Добавлено: Отправка хоста под видом udp-сигнала в классе Сервер. Отправка порта под видом udp-сигнала в классе Сервер. прим.: пока что локалхост.
    Оставлено:
    После отправки сигнала производится подключение по TCP.
    
    Не исправлено:
    Клиент.

--- CliSer/Server.cs
+++ CliSer/Server.cs
@@ -10,11 +10,72 @@ using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
+using System.Threading;
 
 namespace WindowsFormsApp1
 {
+
+    //TCP
     public class Server
     {
+        public void Start(string host = "localhost", int port = 24432)
+        {
+            //порт и хост сигнала равны для теста на локалке. иначе нужно приравнять к порту и хосту этого компьютера
+            var signal_host = Encoding.UTF8.GetBytes(host); 
+            var signal_port = Encoding.UTF8.GetBytes(port.ToString());
+
+            //коннектимся к клиенту, отправляем сигнал для включения TCP-лучей в нашу сторону
+            using (var udpSignal = new UdpClient(host, port)) //создаем UdpClient
+                while (true)
+                {
+                    using (var memoryStream = new MemoryStream())  //создаем временный поток для айпи
+                    {
+                        //отправляем айпи этого компьютера
+                       // memoryStream.WriteByte((byte)(signal_host));
+                       // memoryStream.WriteByte((byte)(signal_port));
+                        //получаем массив байт
+                       // byte[] streamArray = memoryStream.ToArray();
+                        udpSignal.Send(signal_host, signal_host.Length);//отправляем host
+                        Thread.Sleep(10);//если убрать эту задержку, то не все UDP пакеты приходит, почему - хз
+                        udpSignal.Send(signal_port, signal_port.Length);//отправляем host
+                        Thread.Sleep(10);//если убрать эту задержку, то не все UDP пакеты приходит, почему - хз [2]
+                    }
+                }
+
+        }
+
+
+
+
+
+
+
+        /*
+                public IEnumerable<Image> GetScreenshots(int port = 24432)
+                {
+                    var list = new TcpListener(port);
+                    list.Start();
+
+                    using (var tcp = list.AcceptTcpClient())//принимаем конект
+                    using (var stream = tcp.GetStream())//создаем сетевой поток
+                    using (var br = new BinaryReader(stream)) //создаем BinaryReader
+                        while (true)//делаем бесконечно
+                        {
+                            //принимаем длину 
+                            int len = br.ReadInt32();
+                            //принимаем массив
+                            byte[] arr = br.ReadBytes(len);
+                            using (var ms = new MemoryStream(arr))//создаем временный поток для сжатого изображения
+                            {
+                                //создаем изображение
+                                yield return Bitmap.FromStream(ms);
+                            }
+
+                        }
+                }
+
+                */
+
         public IEnumerable<Chunk> GetScreenshots(int port = 24432)
         {
             using (var udp = new UdpClient(port))
@@ -38,41 +99,14 @@ namespace WindowsFormsApp1
                     }
                 }
         }
+    }
 
-        public class Chunk
-        {
-            public Point Position;
-            public Image Image;
-        }
-
-        /* TCP
-          class Server
-                {
-                    public IEnumerable<Image> GetScreenshots(int port = 24432)
-                    {
-                        var list = new TcpListener(port);
-                        list.Start();
-
-                        using (var tcp = list.AcceptTcpClient())//принимаем конект
-                        using (var stream = tcp.GetStream())//создаем сетевой поток
-                        using (var br = new BinaryReader(stream)) //создаем BinaryReader
-                            while (true)//делаем бесконечно
-                            {
-                                //принимаем длину 
-                                int len = br.ReadInt32();
-                                //принимаем массив
-                                byte[] arr = br.ReadBytes(len);
-                                using (var ms = new MemoryStream(arr))//создаем временный поток для сжатого изображения
-                                {
-                                    //создаем изображение
-                                    yield return Bitmap.FromStream(ms);
-                                }
-
-                            }
-                    }
-                }
-         */
-
-
+    public class Chunk
+    {
+        public Point Position;
+        public Image Image;
     }
 }
+
+
+
